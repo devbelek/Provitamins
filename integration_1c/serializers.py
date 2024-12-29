adf5df8496ae326d550eeb3f9304531e5180743e
devbelek <belekasrarov10@gmail.com>
2024-12-29 20:01:07 +0600

    dfdf

--- integration_1c/serializers.py
+++ integration_1c/serializers.py
@@ -8,24 +8,17 @@ class Product1CSerializer(serializers.ModelSerializer):
         model = Product1C
         fields = ('name_en', 'vendor_code', 'price', 'status')
 
-    def validate_vendor_code(self, value):
-        """
-        Проверяем существование товара с таким артикулом
-        """
-        instance = self.instance  # Текущий объект, если это обновление
-        # Проверяем существует ли товар с таким артикулом
-        if Product1C.objects.filter(vendor_code=value).exists():
-            raise serializers.ValidationError("Товар из 1С с таким Артикул уже существует.")
-        return value
-
     def create(self, validated_data):
-        # Проверяем существование товара
+        # Проверяем существование товара по артикулу
         instance = Product1C.objects.filter(
             vendor_code=validated_data['vendor_code']
         ).first()
 
         if instance:
             # Обновляем существующий товар
+            old_price = instance.price  # Сохраняем старую цену для лога
+
+            # Обновляем все поля из validated_data
             for key, value in validated_data.items():
                 setattr(instance, key, value)
             instance.save()
@@ -33,27 +26,58 @@ class Product1CSerializer(serializers.ModelSerializer):
             # Обновляем опубликованный товар если есть
             if instance.published_product:
                 product = instance.published_product
+                product.name = validated_data.get('name_en', product.name)
                 product.price = validated_data.get('price', product.price)
                 product.status = validated_data.get('status', product.status)
                 product.save()
 
-            # Логируем
+            # Логируем обновление
             SyncLog.objects.create(
                 product_1c=instance,
                 sync_type='update',
                 status=True,
-                message=f'Товар обновлен из 1С. Старая цена: {instance.price}, новая цена: {validated_data["price"]}'
+                message=f'Товар обновлен из 1С. Старая цена: {old_price}, новая цена: {instance.price}. '
+                        f'Артикул: {instance.vendor_code}'
             )
 
             return instance
 
-        # Создаем новый товар
-        instance = super().create(validated_data)
+        # Создаем новый товар если не существует
+        instance = Product1C.objects.create(**validated_data)
+
+        # Логируем создание
         SyncLog.objects.create(
             product_1c=instance,
             sync_type='create',
             status=True,
-            message='Товар создан из 1С'
+            message=f'Товар создан из 1С. Артикул: {instance.vendor_code}'
+        )
+
+        return instance
+
+    def update(self, instance, validated_data):
+        old_price = instance.price  # Сохраняем старую цену для лога
+
+        # Обновляем все поля из validated_data
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.save()
+
+        # Обновляем опубликованный товар если есть
+        if instance.published_product:
+            product = instance.published_product
+            product.name = validated_data.get('name_en', product.name)
+            product.price = validated_data.get('price', product.price)
+            product.status = validated_data.get('status', product.status)
+            product.save()
+
+        # Логируем обновление
+        SyncLog.objects.create(
+            product_1c=instance,
+            sync_type='update',
+            status=True,
+            message=f'Товар обновлен из 1С. Старая цена: {old_price}, новая цена: {instance.price}. '
+                    f'Артикул: {instance.vendor_code}'
         )
 
         return instance
