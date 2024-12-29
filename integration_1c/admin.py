d4af4694ece5c74d8ef87f1da14e2b170178c4b3
devbelek <belekasrarov10@gmail.com>
2024-12-29 20:14:05 +0600

    dfdf

--- integration_1c/admin.py
+++ integration_1c/admin.py
@@ -3,6 +3,7 @@ from django.utils.html import format_html
 from .models import Product1C, SyncLog
 from marketplace.models import Product
 
+
 class SyncLogInline(admin.TabularInline):
     model = SyncLog
     extra = 0
@@ -12,6 +13,7 @@ class SyncLogInline(admin.TabularInline):
 
     def has_add_permission(self, request, obj=None):
         return False
+
 
 @admin.register(Product1C)
 class Product1CAdmin(admin.ModelAdmin):
@@ -49,27 +51,20 @@ class Product1CAdmin(admin.ModelAdmin):
             'classes': ('collapse',)
         }),
         ('Публикация', {
-            'fields': (
-                'is_published',
-                'published_status',
-                'published_product'
-            )
+            'fields': ('is_published', 'published_status')
         })
     )
 
-    def get_readonly_fields(self, request, obj=None):
-        if obj and obj.published_product:
-            return self.readonly_fields + ('vendor_code',)
-        return self.readonly_fields
-
     def published_status(self, obj):
-        if obj.published_product:
+        # Проверяем существование товара в основном каталоге
+        if Product.objects.filter(vendor_code=obj.vendor_code).exists():
             return format_html(
                 '<span style="color: green;">✔ Опубликован</span>'
             )
         return format_html(
             '<span style="color: red;">✖ Не опубликован</span>'
         )
+
     published_status.short_description = 'Статус публикации'
 
     actions = ['publish_products', 'unpublish_products']
@@ -77,77 +72,67 @@ class Product1CAdmin(admin.ModelAdmin):
     def publish_products(self, request, queryset):
         for product in queryset:
             if not product.is_published:
-                product.is_published = True
-                product.save()
+                # Создаем новый товар в основном каталоге
+                new_product = Product.objects.create(
+                    name=product.name or product.name_en,
+                    vendor_code=product.vendor_code,
+                    price=product.price,
+                    status=product.status,
+                    description=product.description or "",
+                    brand=product.brand,
+                    manufacturer_country=product.manufacturer_country,
+                    form=product.form,
+                    flavor=product.flavor,
+                    dosage=product.dosage,
+                    sale_price=product.sale_price,
+                    is_hit=product.is_hit,
+                    is_sale=product.is_sale,
+                    is_recommend=product.is_recommend,
+                    quantity=product.quantity or "1",
+                    rating=product.rating,
+                    seo_keywords=product.seo_keywords
+                )
 
-                if product.published_product:
-                    # Обновляем существующий товар
-                    prod = product.published_product
-                else:
-                    # Создаем новый товар
-                    prod = Product(
-                        name=product.name or product.name_en,
-                        vendor_code=product.vendor_code,
-                        price=product.price,
-                        status=product.status,
-                        description=product.description or "",
-                        brand=product.brand,
-                        manufacturer_country=product.manufacturer_country,
-                        form=product.form,
-                        flavor=product.flavor,
-                        dosage=product.dosage,
-                        sale_price=product.sale_price,
-                        is_hit=product.is_hit,
-                        is_sale=product.is_sale,
-                        is_recommend=product.is_recommend,
-                        quantity=product.quantity or "1",
-                        rating=product.rating,
-                        seo_keywords=product.seo_keywords
-                    )
-
-                # Обновляем данные
-                prod.save()
-                product.published_product = prod
-                product.save()
+                # Добавляем категории если есть
+                if hasattr(product, 'categories'):
+                    new_product.categories.set(product.categories.all())
 
                 # Логируем публикацию
                 SyncLog.objects.create(
                     product_1c=product,
                     sync_type='publish',
                     status=True,
-                    message='Товар успешно опубликован'
+                    message='Товар успешно опубликован в основном каталоге'
                 )
+
+                # Удаляем товар из 1С
+                product.delete()
 
     publish_products.short_description = 'Опубликовать выбранные товары'
 
     def unpublish_products(self, request, queryset):
         for product in queryset:
-            if product.is_published:
-                product.is_published = False
-                product.save()
-
-                if product.published_product:
-                    product.published_product.delete()
-                    product.published_product = None
-                    product.save()
+            # Находим соответствующий товар в основном каталоге
+            main_product = Product.objects.filter(vendor_code=product.vendor_code).first()
+            if main_product:
+                main_product.delete()
 
                 # Логируем снятие с публикации
                 SyncLog.objects.create(
                     product_1c=product,
                     sync_type='unpublish',
                     status=True,
-                    message='Товар снят с публикации'
+                    message='Товар удален из основного каталога'
                 )
 
     unpublish_products.short_description = 'Снять с публикации'
 
     def save_model(self, request, obj, form, change):
-        was_published = obj.is_published
-        super().save_model(request, obj, form, change)
-
-        if obj.is_published and not was_published:
-            # Если включили публикацию
+        if obj.is_published:
+            # Если установлен флаг публикации, публикуем товар
             self.publish_products(request, Product1C.objects.filter(pk=obj.pk))
+        else:
+            super().save_model(request, obj, form, change)
 
 
 @admin.register(SyncLog)
