944fff9bf0b4500d00c76169567df32bf2db2bb7
devbelek <belekasrarov10@gmail.com>
2024-12-29 20:08:21 +0600

    dfdf

--- integration_1c/serializers.py
+++ integration_1c/serializers.py
@@ -9,23 +9,32 @@ class Product1CSerializer(serializers.ModelSerializer):
         fields = ('name_en', 'vendor_code', 'price', 'status')
 
     def create(self, validated_data):
-        # Проверяем существование товара по артикулу
-        instance = Product1C.objects.filter(
-            vendor_code=validated_data['vendor_code']
-        ).first()
+        vendor_code = validated_data['vendor_code']
+
+        # Сначала ищем товар в таблице Product1C
+        instance = Product1C.objects.filter(vendor_code=vendor_code).first()
 
         if instance:
-            # Обновляем существующий товар
-            old_price = instance.price  # Сохраняем старую цену для лога
+            old_price = instance.price
 
-            # Обновляем все поля из validated_data
+            # Обновляем товар в 1C
             for key, value in validated_data.items():
                 setattr(instance, key, value)
             instance.save()
 
-            # Обновляем опубликованный товар если есть
+            # Проверяем есть ли связанный опубликованный товар
             if instance.published_product:
                 product = instance.published_product
+            else:
+                # Если нет связанного товара, ищем по артикулу в основной таблице
+                product = Product.objects.filter(vendor_code=vendor_code).first()
+                if product:
+                    # Если нашли, связываем с товаром 1C
+                    instance.published_product = product
+                    instance.save()
+
+            # Если есть связанный товар, обновляем его
+            if product:
                 product.name = validated_data.get('name_en', product.name)
                 product.price = validated_data.get('price', product.price)
                 product.status = validated_data.get('status', product.status)
@@ -36,48 +45,42 @@ class Product1CSerializer(serializers.ModelSerializer):
                 product_1c=instance,
                 sync_type='update',
                 status=True,
-                message=f'Товар обновлен из 1С. Старая цена: {old_price}, новая цена: {instance.price}. '
-                        f'Артикул: {instance.vendor_code}'
+                message=(
+                    f'Товар обновлен. '
+                    f'Старая цена: {old_price}, новая цена: {instance.price}. '
+                    f'Артикул: {instance.vendor_code}. '
+                    f'Основной товар: {"обновлен" if product else "не найден"}'
+                )
             )
 
             return instance
 
-        # Создаем новый товар если не существует
+        # Если товар не найден в 1C, создаем новый
         instance = Product1C.objects.create(**validated_data)
+
+        # Проверяем существует ли товар в основной таблице
+        product = Product.objects.filter(vendor_code=vendor_code).first()
+        if product:
+            # Если существует, связываем с товаром 1C
+            instance.published_product = product
+            instance.save()
+
+            # И обновляем его данные
+            product.name = validated_data.get('name_en', product.name)
+            product.price = validated_data.get('price', product.price)
+            product.status = validated_data.get('status', product.status)
+            product.save()
 
         # Логируем создание
         SyncLog.objects.create(
             product_1c=instance,
             sync_type='create',
             status=True,
-            message=f'Товар создан из 1С. Артикул: {instance.vendor_code}'
-        )
-
-        return instance
-
-    def update(self, instance, validated_data):
-        old_price = instance.price  # Сохраняем старую цену для лога
-
-        # Обновляем все поля из validated_data
-        for key, value in validated_data.items():
-            setattr(instance, key, value)
-        instance.save()
-
-        # Обновляем опубликованный товар если есть
-        if instance.published_product:
-            product = instance.published_product
-            product.name = validated_data.get('name_en', product.name)
-            product.price = validated_data.get('price', product.price)
-            product.status = validated_data.get('status', product.status)
-            product.save()
-
-        # Логируем обновление
-        SyncLog.objects.create(
-            product_1c=instance,
-            sync_type='update',
-            status=True,
-            message=f'Товар обновлен из 1С. Старая цена: {old_price}, новая цена: {instance.price}. '
-                    f'Артикул: {instance.vendor_code}'
+            message=(
+                f'Товар создан в 1C. '
+                f'Артикул: {instance.vendor_code}. '
+                f'Основной товар: {"найден и обновлен" if product else "не найден"}'
+            )
         )
 
         return instance
